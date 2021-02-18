d598545baf989dfcc5ae0f774ae20885b218e22e
Neha Pant <npant03@gmail.com>
2021-02-17 18:28:07 -0800

    Initial Commit

---
+++ InnovationChallenge/InnovationChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  InnovationChallengeApp.swift
+//  InnovationChallenge
+//
+//  Created by Neha Pant on 2/17/21.
+//
+
+import SwiftUI
+
+@main
+struct InnovationChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
