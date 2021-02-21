66ddc27b47554b10b1cb8f4f38349221e03b9111
Aarav Wattal <>
2021-02-21 15:27:33 -0800

    finished login page ui lez go

---
+++ File.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  ProjectPlanetIteration2
+//
+//  Created by Aarav Wattal on 2/21/21.
+//  Copyright © 2021 Aarav Wattal. All rights reserved.
+//
+
+import Foundation
---
+++ InnovationChallengeLoginPage/InnovationChallengeLoginPage/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  InnovationChallengeLoginPage
+//
+//  Created by Aarav Wattal on 2/21/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
