e7192017ac2fee5c82be3d38b880b5b0ff63bae5
Sudharsan Gopalakrishnan <reach2sudharsan@gmail.com>
2021-03-01 19:32:45 -0800

    Commiting files

---
+++ Activize/LoginViewController.swift
@@ -0,0 +1,26 @@
+//
+//  LoginViewController.swift
+//  Activize
+//
+//  Created by Sudharsan Gopalakrishnan on 2/28/21.
+//
+
+//import Foundation
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+
