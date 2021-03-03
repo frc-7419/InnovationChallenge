962fb4e98078c16cb2bf37924064262e20399029
Aarav Sharma <aaravlikestennis@gmail.com>
2021-03-02 19:31:50 -0800

    no important changes

--- Activize/ViewController.swift
+++ Activize/ViewController.swift
@@ -13,5 +13,10 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func didTapButton() {
+        let vc = storyboard?.instantiateViewController(identifier: "signUp") as! SignUpPageVC
+        present(vc, animated: true)
+    }
 }
 
